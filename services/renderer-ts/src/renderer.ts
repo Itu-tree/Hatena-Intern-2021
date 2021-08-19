523d5f67b95008499a1bd99dcd2e10e4ad862cb0
Itu-tree <toutounode@gmail.com>
2021-08-19 12:45:47 +0900

    apply linter

--- services/renderer-ts/src/renderer.ts
+++ services/renderer-ts/src/renderer.ts
@@ -1,16 +1,13 @@
-import unified  from 'unified';
-import markdown  from 'remark-parse';
-import remark2rehype from 'remark-rehype';
-import html from 'rehype-stringify';
+import unified from "unified";
+import markdown from "remark-parse";
+import remark2rehype from "remark-rehype";
+import html from "rehype-stringify";
 
-const processor = unified()
-.use(markdown)
-.use(remark2rehype)
-.use(html);
+const processor = unified().use(markdown).use(remark2rehype).use(html);
 
 /**
  * 受け取った文書を HTML に変換する
  */
 export async function render(src: string): Promise<string> {
-  return  processor.processSync(src).toString();
+  return processor.processSync(src).toString();
 }
